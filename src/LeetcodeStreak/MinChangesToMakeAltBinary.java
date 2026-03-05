58f737d292b51f8dff3971346e54dedf7b6ee4ee
Johnson A J <johnson.07x@gmail.com>
2026-03-05 23:55:01 +0530

    05/03/2026 Leetcode streak update

--- src/LeetcodeStreak/MinChangesToMakeAltBinary.java
+++ src/LeetcodeStreak/MinChangesToMakeAltBinary.java
@@ -2,31 +2,40 @@ package LeetcodeStreak;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
-// to be continued...
 // Leetcode no.1758
 public class MinChangesToMakeAltBinary {
     static public int minOperations(String s) {
-        if (s.length() <= 1) {
+        if (s.length() <= 2) {
             return 0;
         }
-        List<Character> l = new LinkedList<>();
-        int count = 0;
-        l.add(s.charAt(0));
-        for (int i = 1; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            if (ch == l.getLast()) {
-                count++;
-                if (ch == '1') {
-                    l.addLast('0');
-                } else {
-                    l.addLast('1');
-                }
+        String s1 = "";
+        String s2 = "";
+        for (int i = 0; i < s.length(); i++) {
+            if (i % 2 == 0) {
+                s1 += '0';
+                s2 += '1';
             } else {
-                l.addLast(ch);
+                s1 += '1';
+                s2 += '0';
             }
         }
-        return count;
+
+        int min = Integer.MAX_VALUE;
+        int s1count = 0;
+        int s2count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != s1.charAt(i)) {
+                s1count++;
+            }
+            if (s.charAt(i) != s2.charAt(i)) {
+                s2count++;
+            }
+        }
+        min = Math.min(s1count, s2count);
+
+        return min;
     }
 
     static void main(String[] args) {
