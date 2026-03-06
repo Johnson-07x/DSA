da2edf6f115fcb32599dea6364f0c4fa89498688
Johnson A J <johnson.07x@gmail.com>
2026-03-07 02:18:37 +0530

    06/03/2026 LeSuccess Task update

--- src/Leetcode/LinkedList/RotateList.java
+++ src/Leetcode/LinkedList/RotateList.java
@@ -3,19 +3,21 @@ package Leetcode.LinkedList;
 // Leetcode no.61
 public class RotateList {
     static public ListNode rotateRight(ListNode head, int k) {
-        ListNode temp = head;
-        int length = 1;
+        if (head == null || head.next == null || k == 0) {
+            return head;
+        }
         ListNode curr = head;
+        int length = 1;
         while (curr.next != null) {
             curr = curr.next;
             length++;
         }
+
         curr.next = head;
-
         k = k % length;
-        int stepsToCut = length - k;
+        int stepsToNewTail = length - k;
 
-        for (int i = 0; i < stepsToCut; i++) {
+        for (int i = 0; i < stepsToNewTail; i++) {
             curr = curr.next;
         }
 
