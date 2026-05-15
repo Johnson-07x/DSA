dbe82619186912ebe65fb964ce5f4b6f4cc0dabc
Johnson A J <johnson.07x@gmail.com>
2026-05-15 23:36:27 +0530

    15/05/2026 - Leetcode Mission Codind II

--- src/Leetcode/LongestHappyPrefix.java
+++ src/Leetcode/LongestHappyPrefix.java
@@ -1,18 +1,33 @@
 package Leetcode;
 
-
 // Leetcode no.1392
 public class LongestHappyPrefix {
     static public String longestPrefix(String s) {
         int n = s.length();
+        // Base case
+        if (n <= 1) return "";
 
-        for (int i = n - 1; i > 0; i--) {
-            if (s.substring(0, i).equals(s.substring(n - i))) {
-                return s.substring(0, i);
+        int[] lps = new int[n];
+        int len = 0, i = 1;
+
+        while (i < n) {
+            if (s.charAt(i) == s.charAt(len)) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+                if (len != 0) {
+                    len = lps[len - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
             }
         }
 
-        return "";
+        int longestPrefixLen = lps[n - 1];
+
+        return s.substring(0, longestPrefixLen);
     }
 
     static void main(String[] args) {
